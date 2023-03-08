14660648666e9c31d9e47863fd83f10b7994112d
Arjen <aratramba@gmail.com>
2023-03-08 10:17:25 +0100

    Create index.tsx

---
+++ test/index.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import { ApplicationForm } from "../src";
+
+import form from "./fixtures/form";
+const domNode = document.getElementById("app");
+const root = createRoot(domNode);
+root.render(
+  <ApplicationForm formFields={form.form_fields} questions={form.questions} />,
+);
