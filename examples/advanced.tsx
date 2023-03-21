39d18e32b54850dc3682c622235bfe0546d17e33
Arjen <aratramba@gmail.com>
2023-03-21 16:53:51 +0100

    Update advanced.tsx

--- examples/advanced.tsx
+++ examples/advanced.tsx
@@ -28,9 +28,12 @@ const onSave = (
     .then((response) => response.json())
     .then((response) => cb(null))
     .catch((err) => {
-      const error = err.message;
-      const workableErrorMessage = JSON.parse(error.message)?.error;
-      cb(workableErrorMessage || err.message);
+      let errorMessage = err.message;
+      try {
+        errorMessage = JSON.parse(errorMessage)?.error;
+      } catch (err) {}
+
+      cb(errorMessage);
     });
 };
 
@@ -119,21 +122,21 @@ root.render(
         ...(form.questions || []),
 
         // add privacy field
-        {
-          id: "privacy",
-          type: "boolean",
-          required: true,
-          body: (
-            <span>
-              I have read, understand and accept the content of the{" "}
-              <a href="https://google.com" target="_blank">
-                Privacy Notice
-              </a>{" "}
-              and consent to the processing of my data as part of this
-              application.
-            </span>
-          ),
-        },
+        // {
+        //   id: "privacy",
+        //   type: "boolean",
+        //   required: true,
+        //   body: (
+        //     <span>
+        //       I have read, understand and accept the content of the{" "}
+        //       <a href="https://google.com" target="_blank">
+        //         Privacy Notice
+        //       </a>{" "}
+        //       and consent to the processing of my data as part of this
+        //       application.
+        //     </span>
+        //   ),
+        // },
       ]}
       config={{
         telephoneInitialCountry: "IT",
